bcc3a2dfb8ed62178a3ad27fcbf021e3bf3c3d76
motoi sato <ayunoppa@nonoca.net>
2026-01-27 10:03:05 +0900

    user追加 master readonly

---
+++ docker/db/init/init.sql
@@ -0,0 +1,10 @@
+-- create application writable user (master)
+CREATE USER IF NOT EXISTS 'master'@'%' IDENTIFIED BY 'master';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER
+  ON `cake`.* TO 'master'@'%';
+
+-- create read-only user (replica/app readonly)
+CREATE USER IF NOT EXISTS 'readonly'@'%' IDENTIFIED BY 'readonly';
+GRANT SELECT ON `cake`.* TO 'readonly'@'%';
+
+FLUSH PRIVILEGES;
