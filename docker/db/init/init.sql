20614240158e78ac9c4b63f0c15ae17ebad3e764
motoi <sato_motoi@ftgroup.co.jp>
2026-01-30 22:52:09 +0900

    DB User PERERENCES追加

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -1,6 +1,6 @@
 -- create application writable user (master)
 CREATE USER IF NOT EXISTS 'master'@'%' IDENTIFIED BY 'master';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, REFERENCES
   ON `cake`.* TO 'master'@'%';
 
 -- create read-only user (replica/app readonly)
